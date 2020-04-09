5bf2e4a32e3d995078f759398333b4e3a07be71f
Kieran Zylstra <kieran.zylstra@colorado.edu>
2020-04-08 19:27:00 -0600

    connecting to database now

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+#this
 
 app = Flask(__name__) # NAME OF FILE
+app.config['SQLALCHEMY_DATABASE_URI'] = '''postgres://postgres:Kieran@localhost:5432/todoapp'''
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tbalename__ = 'todos'
+    id= db.Column(db.Integer, primary_key=True)
+    description =db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
